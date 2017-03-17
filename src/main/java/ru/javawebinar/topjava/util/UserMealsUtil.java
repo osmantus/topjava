b17c6fee9424ded7e5f2757267705055d5ce728a
osmantus <poznanski.alex@gmail.com>
2017-03-18 01:55:40 +0200

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -29,7 +29,6 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        System.out.println("TODO return filtered list with correctly exceeded field");
 
         List<UserMealWithExceed> resultedList = new ArrayList<>();
         Map<LocalDate, Integer> totalMealCaloriesByDate = new HashMap<>();
