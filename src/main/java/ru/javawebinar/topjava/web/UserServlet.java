3e7c5f12f4d711dd990340bc55ea17fe8665c6cf
osmantus <poznanski.alex@gmail.com>
2017-04-10 02:00:19 +0300

    2 3 app layers

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,7 +14,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  * User: gkislin
  * Date: 19.08.2014
  */
-public class UserServlet {
+public class UserServlet extends HttpServlet {
     private static final Logger LOG = getLogger(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
