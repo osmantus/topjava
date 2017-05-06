411e87f201cc0fd68c99bcd64d302155e3dcf117
osmantus <poznanski.alex@gmail.com>
2017-05-07 00:20:45 +0300

    5_10 app layers

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -15,27 +15,27 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
     private CrudMealRepository crudRepository;
 
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    public Meal save(Meal meal, int userId) {
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.findByUserIdAndId(userId, id);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudRepository.getBetween(userId, startDate, endDate);
     }
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -6,8 +6,18 @@ ALTER SEQUENCE global_seq RESTART WITH 100000;
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password');
 
-INSERT INTO users (name, email, password)
-VALUES ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password) VALUES
+  ('Admin', 'admin@gmail.com', 'admin'),
+  ('Admin1', 'admin1@gmail.com', 'admin1'),
+  ('Admin2', 'admin2@gmail.com', 'admin2'),
+  ('User1', 'user1@gmail.com', 'user1'),
+  ('User2', 'user2@gmail.com', 'user2'),
+  ('User3', 'user3@gmail.com', 'user3'),
+  ('User4', 'user4@gmail.com', 'user4'),
+  ('User5', 'user5@gmail.com', 'user5'),
+  ('User6', 'user6@gmail.com', 'user6'),
+  ('User7', 'user7@gmail.com', 'user7'),
+  ('User8', 'user8@gmail.com', 'user8');
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
@@ -21,4 +31,4 @@ INSERT INTO meals (date_time, description, calories, user_id) VALUES
   ('2015-05-31 13:00:00', 'Обед', 1000, 100000),
   ('2015-05-31 20:00:00', 'Ужин', 510, 100000),
   ('2015-06-01 14:00:00', 'Админ ланч', 510, 100001),
-  ('2015-06-01 21:00:00', 'Админ ужин', 1500, 100001);
+  ('2015-06-01 21:00:00', 'Админ ужин', 1500, 100002);
